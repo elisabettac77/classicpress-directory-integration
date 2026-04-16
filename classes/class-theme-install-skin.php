dfd660f31e9e01888ac33987f37e6d77c79f0b78
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2026-04-16 14:57:23 +0200

    Create ThemeInstallSkin class for theme installation
    Create ThemeInstallSkin class for theme installation

---
+++ classes/class-theme-install-skin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ClassicPress\Directory;
+
+class ThemeInstallSkin extends \Theme_Installer_Skin {
+	public function header() {
+	}
+	public function footer() {
+	}
+	public function error( $errors ) {
+	}
+	public function feedback( $string, ...$args ) {
+	}
+}
