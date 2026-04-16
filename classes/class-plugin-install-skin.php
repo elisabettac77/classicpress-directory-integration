5601bd40928743de7a3d49490765ee5b4c91d78b
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2026-04-16 14:56:44 +0200

    Add PluginInstallSkin class for plugin installation
    Add PluginInstallSkin class for plugin installation

---
+++ classes/class-plugin-install-skin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ClassicPress\Directory;
+
+class PluginInstallSkin extends \Plugin_Installer_Skin {
+	public function header() {
+	}
+	public function footer() {
+	}
+	public function error( $errors ) {
+	}
+	public function feedback( $string, ...$args ) {
+	}
+}
