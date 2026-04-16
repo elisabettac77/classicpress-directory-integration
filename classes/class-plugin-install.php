2c769b7580fff3f99cc6c4fbc9e9ce7766f84762
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2026-04-16 16:22:05 +0200

    Enhance plugin menu creation and renaming
    Updated create_menu and rename_menu methods to include submenu page creation and action hooks for plugin installation.

--- classes/class-plugin-install.php
+++ classes/class-plugin-install.php
@@ -8,13 +8,39 @@ class PluginInstall extends Abstract_Install {
 		parent::__construct();
 	}
 
-	public function create_menu() {
-		if ( ! current_user_can( 'install_plugins' ) ) { return; }
-		// ... add_submenu_page logic using 'plugins.php' and 'install_plugins'
+		public function create_menu() {
+		if ( ! current_user_can( 'install_plugins' ) ) {
+			return;
+		}
+
+		$this->page = add_submenu_page(
+			'plugins.php',
+			esc_html__( 'Install ClassicPress Plugins', 'classicpress-directory-integration' ),
+			esc_html__( 'Install CP Plugins', 'classicpress-directory-integration' ),
+			'install_plugins',
+			'classicpress-directory-integration-plugin-install',
+			array( $this, 'render_menu' ),
+			2
+		);
+
+		add_action( 'load-' . $this->page, array( $this, 'activate_action' ) );
+		add_action( 'load-' . $this->page, array( $this, 'install_action' ) );
 	}
 
 	public function rename_menu() {
-		// ... plugin specific menu renaming
+		if ( ! current_user_can( 'install_plugins' ) ) {
+			return;
+		}
+
+		global $submenu;
+		if ( isset( $submenu['plugins.php'] ) ) {
+			foreach ( $submenu['plugins.php'] as $key => $value ) {
+				if ( $value[2] !== 'plugin-install.php' ) {
+					continue;
+				}
+				$submenu['plugins.php'][ $key ][0] = esc_html__( 'Install WP Plugins', 'classicpress-directory-integration' ); // phpcs:ignore
+			}
+		}
 	}
 
 	protected function get_local_cp_items() {
