953a4c26854ce9a329ccaa0b2ea87af36d085d88
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2026-04-21 15:38:11 +0200

    Refactor plugin update logic for compatibility
    correct CPCS errors

--- classes/class-plugin-update.php
+++ classes/class-plugin-update.php
@@ -35,15 +35,16 @@ class Plugin_Update extends Abstract_Update {
 			return $this->cp_items;
 		}
 
-		$all_plugins     = get_plugins();
+		$all_plugins    = get_plugins();
 		$this->cp_items = array();
-		$directory_host  = wp_parse_url( \CLASSICPRESS_DIRECTORY_INTEGRATION_URL, PHP_URL_HOST );
+		$directory_host = wp_parse_url( \CLASSICPRESS_DIRECTORY_INTEGRATION_URL, PHP_URL_HOST );
 
 		foreach ( $all_plugins as $file => $data ) {
 			$update_uri = $data['UpdateURI'] ?? '';
 
 			// If the UpdateURI matches our directory host, it's one of ours.
-			if ( ! empty( $update_uri ) && str_contains( $update_uri, $directory_host ) ) {
+			// Using strpos for PHP 7.4 compatibility instead of str_contains.
+			if ( ! empty( $update_uri ) && false !== strpos( $update_uri, $directory_host ) ) {
 				$slug                    = dirname( $file );
 				$this->cp_items[ $slug ] = $data;
 			}
@@ -54,8 +55,13 @@ class Plugin_Update extends Abstract_Update {
 
 	/**
 	 * Filter the update response for CP Directory plugins.
+	 * * @param array|bool  $update    The update data, or false.
+	 * @param array       $item_data The item data.
+	 * @param string      $item_file The item file path.
+	 * @param array       $locales   The locales.
+	 * @return array|bool
 	 */
-	public function update_uri_filter( array|false $update, array $item_data, string $item_file, array $locales ): array|false {
+	public function update_uri_filter( $update, array $item_data, string $item_file, array $locales ) {
 		$slug = dirname( $item_file );
 		$data = $this->get_directory_data();
 
